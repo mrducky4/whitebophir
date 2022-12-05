6625027194ed6a5e3546138e4cbf8b935648ad0f
Mark <mark@avarobotics.com>
2022-12-05 12:35:46 -0500

    AFE-212 increase whiteboard capture delay to wait for markers to appear. Do not write passwords to log file.

--- get_snapshot_markers.sh
+++ get_snapshot_markers.sh
@@ -4,7 +4,12 @@ set -e # exit on error
 boardcode=$5
 markers_filename="${boardcode}_snapshot_markers.jpg"
 
-sleep 0.3
+# Wait long enough for the markers to appear on the projected
+# image. Testing shows 0.5 is long enough. But it is possible
+# it might need to be more at some point, for example if a high
+# latency network connection across the world causes additional
+# delay in the messages to the client apps.
+sleep 0.5
 ./get_snapshot.sh $1 $2 $3 $4 > "xform/${markers_filename}"
 
 echo "Done getting snapshot with markers for whiteboard code ${boardcode}"
